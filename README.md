fd51828a51bac8b53a699800cc5e378f5eeb68a9
Jhonatan Mejia <quenandres@gmail.com>
2022-11-04 15:00:19 -0500

    useRef

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# Para qué sirve useRef, useMemo, useCallback, diferencia con useState ? NO SIEMPRE HAY QUE USARLOS !
+### Para qué sirve useRef, useMemo, useCallback, diferencia con useState ? NO SIEMPRE HAY QUE USARLOS !
 
 React utiliza virtual DOM, cada vez que se haga un render, compara la copia anterior del DOM con el estado actual del DOM para cambiar unicamente la que tuvo el cambio.
 
 El cambio de useEffect solo se hace presente cuando cambia las depencias.
+
+#### useRef
+Referenciamos un elemento.
+```js
+<button ref={refButton} onClick={handleClick}>Click here to change name</button>
+<button onClick={() => refButton.current.click()}>Click here to other button clone</button>
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
   const [name, setName] = useState('gentleman');
+  const refButton = useRef(null);
   
   useEffect(() => {  
     console.log('rendered');
     return () => {} // Cuando el componente se destruya
   }, [name]);
+
+  const handleClick = () => {
+    setName('Alan');
+    if( name !== 'Gentleman' ) refButton.current.style.color = 'red';
+  }
   
 
   return (
     <div className="App">
       <h1>{name}</h1>
-      <button onClick={() => setName('Alan')}>Click here to change name</button>
+      <button ref={refButton} onClick={handleClick}>Click here to change name</button>
+      <button onClick={() => refButton.current.click()}>Click here to other button clone</button>
     </div>
   );
 }
